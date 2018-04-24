d8362b4d646edafc208e08f206e85348da5e6516
Jonathan Roesner <john4rrr@yahoo.de>
2018-04-24 21:04:25 +0200

    add comments

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# remove lock files to prevent apt-get update from failing
 sudo rm /var/cache/apt/archives/lock
 sudo rm /var/lib/dpkg/lock
+# install docker-ce
 sudo apt-get update
 sudo apt-get install \
     apt-transport-https \
@@ -14,7 +16,12 @@ sudo add-apt-repository \
     stable"
 sudo apt-get update
 sudo apt-get install docker-ce -y
+# install docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+# increase virtual memory size for elasticsearch
 sudo sysctl -w vm.max_map_count=262144
-docker-compose up -d
+# accept incoming messages for kafka
+iptables -I INPUT -p tcp -m tcp --dport 9092 -j ACCEPT
+# start all containers
+docker-compose up
